d735dc5770c4b9920fa591bed465c29eee266aee
brandoncc94 <brandoncc94@gmail.com>
2015-11-11 15:50:59 -0600

    Changes

---
+++ src/maininterface/Connection.java
@@ -0,0 +1,8 @@
+
+package maininterface;
+
+public class Connection {
+    public static final String RMI_IP = "127.0.0.1";
+    public static final int RMI_PORT = 1099;
+    public static final String RMI_ID = "FileSystem";
+}
