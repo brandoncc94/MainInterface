434e7a77efe23e26b5710f1c4598315803526301
Juan Carlos Martinez <juancar199400@gmail.com>
2015-11-11 22:05:12 -0600

    Agregado CD y mkdir. Consola por comando.

--- src/maininterface/IFunctions.java
+++ src/maininterface/IFunctions.java
@@ -6,7 +6,11 @@ import java.rmi.RemoteException;
 
 public interface IFunctions extends Remote{
     //Función para crear espacio virtual en el disco
-    void create(int pSize) throws RemoteException;
+    String create(int pSize) throws RemoteException;
+    //Función para obtener la ubicacion actual en el directorio
+    String getPath(String root) throws RemoteException;
     //Función para crear un directorio
-    void MKDIR(String pName) throws RemoteException;
+    void mkdir(String pName,String pRoot) throws RemoteException;
+    //Función cd
+    boolean cd(String pNewPath,String pRoot) throws RemoteException;
 }
